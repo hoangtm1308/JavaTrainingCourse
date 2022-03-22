f14598fb965dadbdc96e0d2d04f83c2e1abfe54a
hoangtm <hoangtm1308@gmail.com>
2022-03-22 14:39:13 +0700

    OOP Trainning caculator

--- OOPTraining/src/Main.java
+++ OOPTraining/src/Main.java
@@ -3,5 +3,21 @@ public class Main {
         Employee hoang = new Employee(001, "Manager", 2000, "Home");
         hoang.setEmployeeSalary(3000);
         hoang.getInfo();
+        Car porsche = new Car();
+        Car holden = new Car();
+        Car kia = new Car();
+        porsche.setModel("Carrera");
+        kia.setModel("Holden");
+        System.out.println(kia.getModel());
+        System.out.println(porsche.getModel());
+        SimpleCalculator calculator = new SimpleCalculator();
+        calculator.setFirstNumber(5.0);
+        calculator.setSecondNumber(4);
+        System.out.println("add= " + calculator.getAdditionResult());
+        System.out.println("subtrat= " + calculator.getSubtractionResult());
+        calculator.setFirstNumber(5.25);
+        calculator.setSecondNumber(0);
+        System.out.println("multiply= " + calculator.getMultiplicationResult());
+        System.out.println("division= " + calculator.getDivisionResult());
     }
 }
